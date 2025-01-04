e8a6a319edf61d651e528f48b40b1295a57878fa
Ismael Saavedra <16451895+Archweii@users.noreply.github.com>
2025-01-04 15:24:55 +0100

    Set device class C

--- examples/miut_otaa/config.h
+++ examples/miut_otaa/config.h
@@ -5,7 +5,7 @@
  * §
  */
 
-#define LORAWAN_DEFAULT_CLASS   CLASS_A
+#define LORAWAN_DEFAULT_CLASS   CLASS_C
 
 // LoRaWAN region to use, full list of regions can be found at: 
 //   http://stackforce.github.io/LoRaMac-doc/LoRaMac-doc-v4.5.1/group___l_o_r_a_m_a_c.html#ga3b9d54f0355b51e85df8b33fd1757eec
